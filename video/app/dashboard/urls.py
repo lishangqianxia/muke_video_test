c70934ca5e011ca1c5ddf1880fde57b7a66749c3
离上乾下 <62366268+lishangqianxia@users.noreply.github.com>
2020-04-10 20:27:44 +0800

    增加登录验证

--- video/app/dashboard/urls.py
+++ video/app/dashboard/urls.py
@@ -7,7 +7,7 @@ from .views.auth import Login
 from .views.ceshi import Ceshi
 
 urlpatterns = [
-    path('', Index.as_view()),
+    path('', Index.as_view(), name='dashboard_index'),
     path('login', Login.as_view(), name='dashboard_login'),
     path('ceshi', Ceshi.as_view())
 
--- video/app/dashboard/views/auth.py
+++ video/app/dashboard/views/auth.py
@@ -1,7 +1,9 @@
 # coding;utf-8
 
 from django.views.generic import View
-from django.shortcuts import redirect
+from django.shortcuts import redirect, reverse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 from app.libs.base_render import render_to_response
 
 
@@ -17,5 +19,22 @@ class Login(View):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        print(username, password)
-        return redirect('/dashboard/login')
+        data = {}
+
+        exists = User.objects.filter(username=username).exists()
+        data['error'] = '没有该用户'
+
+        if not exists:
+            return render_to_response(request, self.TEMPLATE, data)
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            data['error'] = '密码错误'
+            return render_to_response(request, self.TEMPLATE, data)
+
+        if not user.is_superuser:
+            data['error'] = '你无权登录'
+            return render_to_response(request, self.TEMPLATE, data)
+
+        return redirect(reverse('dashboard_index'))
