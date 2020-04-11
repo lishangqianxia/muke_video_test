fbf021994e1119f738e69dc162a2e3bc6f5f1857
离上乾下 <62366268+lishangqianxia@users.noreply.github.com>
2020-04-11 18:01:51 +0800

    增加管理员页面

--- video/app/dashboard/urls.py
+++ video/app/dashboard/urls.py
@@ -3,12 +3,14 @@
 from django.contrib import admin
 from django.urls import path
 from .views.base import Index
-from .views.auth import Login
+from .views.auth import Login, Logout, AdminManger, UpdateAdminStatus
 from .views.ceshi import Ceshi
 
 urlpatterns = [
     path('', Index.as_view(), name='dashboard_index'),
-    path('login', Login.as_view(), name='dashboard_login'),
-    path('ceshi', Ceshi.as_view())
-
+    path('login', Login.as_view(), name='login'),
+    path('logout', Logout.as_view(), name='logout'),
+    path('ceshi', Ceshi.as_view()),
+    path('admin/manger', AdminManger.as_view(), name='admin_manger'),
+    path('admin/manger/update/status', UpdateAdminStatus.as_view(), name='admin_update_status')
 ]
--- video/app/dashboard/views/auth.py
+++ video/app/dashboard/views/auth.py
@@ -2,8 +2,9 @@
 
 from django.views.generic import View
 from django.shortcuts import redirect, reverse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from app.libs.base_render import render_to_response
 
 
@@ -12,7 +13,11 @@ class Login(View):
 
     def get(self, request):
 
-        return render_to_response(request, self.TEMPLATE)
+        if request.user.is_authenticated:
+            return redirect(reverse('dashboard_index'))
+
+        data = {'error': ''}
+        return render_to_response(request, self.TEMPLATE, data=data)
 
     def post(self, request):
 
@@ -31,10 +36,43 @@ class Login(View):
 
         if not user:
             data['error'] = '密码错误'
-            return render_to_response(request, self.TEMPLATE, data)
+            return render_to_response(request, self.TEMPLATE, data=data)
 
         if not user.is_superuser:
             data['error'] = '你无权登录'
-            return render_to_response(request, self.TEMPLATE, data)
+            return render_to_response(request, self.TEMPLATE, data=data)
+
+        login(request, user)
 
         return redirect(reverse('dashboard_index'))
+
+
+class Logout(View):
+
+    def get(self, request):
+        logout(request)
+        return redirect(reverse('login'))
+
+
+class AdminManger(View):
+    TEMPLATE = 'dashboard/auth/admin.html'
+
+    def get(self, request):
+
+        users = User.objects.all()
+        data = {'users': users}
+
+        return render_to_response(request, self.TEMPLATE, data=data)
+
+
+class UpdateAdminStatus(View):
+
+    def get(self, request):
+
+        status = request.GET.get('status', 'on')
+
+        _status = True if status == 'on' else False
+        request.user.is_superuser = _status
+        request.user.save()
+
+        return redirect(reverse('admin_manger'))
